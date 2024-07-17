42e47b81771dbccf2c535ed6ded456fddead5732
Sarah Schilling <105629757+SarahS92@users.noreply.github.com>
2024-07-17 16:46:57 +0200

    Update deduplicate.js

--- deduplicate.js
+++ deduplicate.js
@@ -1,17 +1,17 @@
 // src/action.js
 var hubspot = require("@hubspot/api-client");
 exports.main = async (event, callback) => {
-  const ID = event["inputFields"]["id"];
+  const ID = event["inputFields"]["id"]; //adjust for your input field name instead of "id"
   let hasDuplicate = false;
   const hubspotClient = new hubspot.Client({
-    accessToken: process.env.APIKEY
+    accessToken: process.env.APIKEY //put your Secret name instead of "APIKEY"
   });
   try {
     const apiResponse = await hubspotClient.crm.contacts.searchApi.doSearch({
-      filterGroups: [{
+      filterGroups: [{ //adjust for your field name
         "filters": [{
-          "value": ID,
-          "propertyName": "salesforceaccountid",
+          "value": ID, 
+          "propertyName": "id",
           "operator": "EQ"
         }]
       }]
@@ -26,6 +26,6 @@ exports.main = async (event, callback) => {
     e.message === "HTTP request failed" ? console.error(JSON.stringify(e.response, null, 2)) : console.error(e);
   }
   callback({
-    outputFields: { has_duplicate: hasDuplicate }
+    outputFields: { has_duplicate: hasDuplicate } //don't forget to define output below custom code
   });
 };
